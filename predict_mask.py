8bc65764b2cbaaa0c9713467f67bbb75be7233ca
Serdar AKYOL <36725883+serdarakyol@users.noreply.github.com>
2020-11-15 13:34:54 +0300

    Update predict_mask.py
    Explanation of code

--- predict_mask.py
+++ predict_mask.py
@@ -1,3 +1,9 @@
+"""
+Serdar AKYOL
+Youtube: https://bit.ly/Mask_Detection
+LinkedIn: https://www.linkedin.com/in/serdarakyol55/
+"""
+
 from cv2 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -12,27 +18,27 @@ labels = ["With mask", "Without mask"]
 colors = [(0,255,0),(0,0,255)]
 while 1:
     _, frame = cap.read()
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(gray_frame, 1.1, 5)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert gray scala for get better result from haar cascade
+    face = face_cascade.detectMultiScale(gray_frame, 1.1, 5) #get face coordinates
     
     if face in np.array(face):
         for (x, y, w, h) in face:
-            try:
-                img_RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                img_roi = img_RGB[y-25:y+h+25, x-25:x+w+25]
-                img_roi = cv2.resize(img_roi, (224, 224), interpolation= cv2.INTER_AREA)
-                img_roi = preprocess_input(img_roi)
-                img_roi = np.reshape(img_roi,(1,224,224,3))
-                pred = model.predict(img_roi)
-                pred_index = np.argmax(pred)
+            try: # I used try-except because when there is no face in frame, then givin None result and Its stops projects. For fix this
+                img_RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #convert BGR image to RGB because I trained my model in RGB
+                img_roi = img_RGB[y-25:y+h+25, x-25:x+w+25] # crop little bit large zone than face because some train images was bigger than only face 
+                img_roi = cv2.resize(img_roi, (224, 224), interpolation= cv2.INTER_AREA) # reshaping cropped image because I trained on 224x244x3 shape
+                img_roi = preprocess_input(img_roi) # normalize cropped data between -1 and 1
+                img_roi = np.reshape(img_roi,(1,224,224,3)) # reshape for prediction
+                pred = model.predict(img_roi) # get predictions
+                pred_index = np.argmax(pred) # get higher result of prediction
                 #print(pred)
                 print(img_roi)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 1)
-                cv2.putText(frame, labels[pred_index], (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, colors[pred_index], 2)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 1) #draw rectangle
+                cv2.putText(frame, labels[pred_index], (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, colors[pred_index], 2) #write result
             except Exception as e:
                 print(str(e))
     cv2.imshow("Frame", frame)
-    gc.collect()
+    gc.collect() # for collect memory
     k = cv2.waitKey(30) & 0xff
     
     if k == 27:
